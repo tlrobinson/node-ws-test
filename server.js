ffccd0e77193db46db66c6d5f0a6535620171f5c
Tom Robinson <tlrobinson@gmail.com>
2013-10-08 15:31:08 -0700

    benchmark mode

--- server.js
+++ server.js
@@ -14,14 +14,11 @@ console.log('http server listening on %d', port);
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
-
-    console.log('websocket connection open');
-
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
-    });
+  console.log('websocket connection open');
+  ws.on('message', function (message) {
+    ws.send(message);
+  });
+  ws.on('close', function() {
+    console.log('websocket connection close');
+  });
 });
